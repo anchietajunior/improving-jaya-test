0066d3c690f0bf6fdb6f83b1ea0f2aa4d30b4d18
Anchieta Júnior <santosjr87@gmail.com>
2019-03-21 23:58:19 -0300

    Update events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,9 @@ class EventsController < ApplicationController
   before_action :authenticate_user, only: [:list]
 
   def new
-    Events::EventCreatorService.call(@request_body) if @webhook_authentication.success?
+    request.body.rewind
+    request_body = request.body.read
+    Events::EventCreatorService.call(request_body) if @webhook_authentication.success?
     head :ok
   end
 
@@ -21,9 +23,7 @@ class EventsController < ApplicationController
   private
 
   def webhook_authenticate
-    request.body.rewind
-    @request_body = request.body.read
-    @webhook_authentication = Authentication::AuthenticationService.call(request.headers['X-Hub-Signature'], @request_body)
+    @webhook_authentication = Authentication::AuthenticationService.call(request.headers['X-Hub-Signature'], request_body)
   end
 
   def authenticate_user
